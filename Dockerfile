4f73e63b6205c132fdabe7d8946f2ceb1d37bf5c
nicoprofe <nicolasportu@gmail.com>
2024-05-20 09:42:32 -0300

    fist commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.10-slim
+
+WORKDIR /python-docker
+
+COPY requirements.txt requirements.txt
+
+RUN apt-get update && apt-get install git -y
+
+RUN pip install -r requirements.txt
+
+RUN pip install --no-cache-dir git+https://github.com/openai/whisper.git 
+
+RUN apt-get update && apt-get install -y ffmpeg
+
+COPY . .
+
+EXPOSE 8000
+
+CMD [ "uvicorn", "fastapi_app:app", "--host", "0.0.0.0", "--port", "8000" ]
---
+++ fastapi_app.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from typing import List
+from fastapi.responses import JSONResponse, RedirectResponse
+import whisper
+import torch
+from tempfile import NamedTemporaryFile
+
+# Check if CUDA is available and set the device accordingly
+torch.cuda.is_available()
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Load the Whisper model
+model = whisper.load_model("base", device=DEVICE)
+
+app = FastAPI()
+
+@app.post("/whisper_local")
+async def transcribe_audio(files: List[UploadFile] = File( ... )):
+    if not files:
+        raise HTTPException(status_code=400, detail="Only one file is allowed")
+    
+    results = []
+
+    for file in files:
+        with NamedTemporaryFile(delete=True) as temp:
+            with open(temp.name, "wb") as temp_file:
+                temp_file.write(file.file.read())
+
+            result = model.transcribe(temp.name)
+
+            results.append(
+                {
+                    "filename": file.filename,
+                    "transcript": result["text"]
+                }
+            )
+
+    return JSONResponse(content={'results': results})
+
+
+@app.get("/", response_class=RedirectResponse)
+async def redirect_to_docs():
+    return "/docs"
